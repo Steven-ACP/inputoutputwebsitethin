7d1dc72a3edd781b02c476e0e761c5a85a469b46
Steven <127scastropacheco@frhsd.com>
2025-12-03 12:50:07 -0500

    jkjkmj

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,57 @@
 /** @format */
+//
+let urName = document.getElementById("nameInput");
+//
+let urAge = document.getElementById("ageInput");
+//
+let locationDrop = document.getElementById("locationInput");
+//
+let submitButton = document.getElementById("submitBtn");
+//
+let raDio = document.querySelectorAll("input[type='radio']");
+//
 
-$('#fancy-inputs input[type="text"]').blur(function () {
-  if ($(this).val().length > 0) {
-    $(this).addClass("white");
-  } else {
-    $(this).removeClass("white");
-  }
-});
+submitButton.addEventListener("click", button);
 
-// Radios
+function button() {
+  let inputName = document.getElementById("nameInput");
+  let inputAge = document.getElementById("ageInput");
+  let inputLocation = document.getElementById("locationInput");
+  let value = inputName.value;
+  let ageValue = inputAge.value;
+  let locationValue = inputLocation.value;
+  document.getElementById("outPut").textContent =
+    "Your Name:" +
+    value +
+    "Your Age:" +
+    ageValue +
+    "Where you live:" +
+    locationValue;
+}
+//function button() {
+//  let inputName = document.getElementById("nameInput");
+//  let inputAge = document.getElementById("ageInput");
+//  let inputlocation = document.getElementById("locationInput");
+//  let inputRadio = document.getElementById("input[type='radio']");
+/////////////////////////
+//  let value = inputName.value;
+// let ageValue = inputAge.value;
+// let locationValue = inputlocation.value;
+// let radioValue = inputRadio.value;
+//document.getElementById("outPut").textContent =
+// "Your Name: " +
+// value +
+// "      Your Age: " +
+// ageValue +
+// "         Favorite location: " +
+// locationValue +
+// "Your favorite animal" +
+// radioValue;
+//}
 
-$("#fancy-radio input[type=radio]").click(function () {
-  $("label.radio").removeClass("selected");
-  var inputID = $(this).attr("id");
-  if ($(this).is(":checked")) {
-    $("." + inputID).addClass("selected");
-  } else {
-    $("." + inputID).removeClass("selected");
-  }
-});
+const names = ["Dog", "Cat", "Fish"];
+
+for (let i = 0; i < names.length; i++) {
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+}
