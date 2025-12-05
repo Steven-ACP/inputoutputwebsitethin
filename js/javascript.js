8f27f0dde4f5f17299713615384251df8c1b9431
Steven <127scastropacheco@frhsd.com>
2025-12-05 13:43:53 -0500

    final update

--- js/javascript.js
+++ js/javascript.js
@@ -18,15 +18,29 @@ function button() {
   let inputName = document.getElementById("nameInput");
   let inputAge = document.getElementById("ageInput");
   let inputLocation = document.getElementById("locationInput");
+  let inputPet = document.getElementById("petInput");
+  let inputJob = document.getElementById("jobInput");
   // let inputRadio = document.getElementById('[type="radio"]');
   // let inputPet = document.getElementById('type="checkbox"]');
   let value = inputName.value;
   let ageValue = inputAge.value;
   let locationValue = inputLocation.value;
+  let petValue = inputPet.value;
+  let jobValue = inputJob.value;
+
   // let radioValue = inputRadio.value;
   /// let petValue = inputPet.value;
   document.getElementById("outPut").textContent =
-    "Name:" + value + "Age:" + ageValue + "Where you live:" + locationValue;
+    "Name:" +
+    value +
+    "Age:" +
+    ageValue +
+    "Where you live:" +
+    locationValue +
+    "Your pet:" +
+    petValue +
+    "Your Job:" +
+    jobValue;
   //  "Your Job(s)" +
   // radioValue +
   // "Your Pet" +
